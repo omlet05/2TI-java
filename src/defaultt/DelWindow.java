bea9abb0753664a407e3bc5ffe2588c253c42f59
Om <Om@pc-omlet-w8_1>
2014-04-23 02:25:01 +0200

    lot of changes

--- src/defaultt/DelWindow.java
+++ src/defaultt/DelWindow.java
@@ -10,10 +10,15 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 
 public class DelWindow extends JFrame {
 
@@ -25,7 +30,8 @@ public class DelWindow extends JFrame {
 	public DelWindow() {
 		setTitle("Supression d'un software.");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setSize(771, 481);
+		setSize(771, 500);
+		setResizable(false);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -42,35 +48,53 @@ public class DelWindow extends JFrame {
 			e.printStackTrace();
 			dispose();
 		}
+		comboBox.addActionListener (new ActionListener () {
+		    public void actionPerformed(ActionEvent e) {
+		        updateTable();
+		    }
+		});
 		
-		
-		comboBox.setBounds(408, 12, 233, 20);
+		comboBox.setBounds(514, 10, 231, 20);
 		contentPane.add(comboBox);
 		
-		JButton btnFiltrer = new JButton("Filtrer");
-		btnFiltrer.setBounds(665, 11, 80, 23);
-		btnFiltrer.addActionListener(new Filter());
-		contentPane.add(btnFiltrer);
+		
 		
 		
 		//table
-			table = new JTable(null);
-			
-			//table.setFillsViewportHeight(true);
-			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-			table.setBackground(Color.white);
-			//JscrollPane pane
-			scrollPane = new JScrollPane(table);			
-			scrollPane.setBounds(10, 51, 735, 380);
-			contentPane.add(scrollPane);
+		table = new JTable(null);
+		
+		//table.setFillsViewportHeight(true);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setBackground(Color.white);
+		//JscrollPane pane
+		scrollPane = new JScrollPane(table);			
+		scrollPane.setBounds(10, 43, 735, 375);
+		contentPane.add(scrollPane);
+		
+		JButton btnSupprimer = new JButton("Supprimer la selection");
+		btnSupprimer.addActionListener(new Del());
+		btnSupprimer.setBounds(608, 429, 137, 23);
+		contentPane.add(btnSupprimer);
+		
+		JLabel lblEditeur = new JLabel("Editeur:");
+		lblEditeur.setBounds(458, 13, 46, 14);
+		contentPane.add(lblEditeur);
+		
+		//add setText to the JtextField by selection
+		/*table.getSelectionModel().addListSelectionListener((new ListSelectionListener(){
+			public void valueChanged(ListSelectionEvent event) {
+				int row = (int) table.getValueAt(table.getSelectedRow(), 0);
+				//textField.setText(String.valueOf(row));
+			}
+		}));*/
 
 		setVisible(true);
 	}
 	
-	
-	private class Filter implements ActionListener{
+	private class Del implements ActionListener{
  		public void actionPerformed(ActionEvent e){
+ 			deleteRow();
  			updateTable();
  		}
  	}
@@ -89,11 +113,38 @@ public class DelWindow extends JFrame {
 		
 	}
 	
+	private void deleteRow(){ //must add exception if null
+            try {
+            	int idInstall = -1;
+            	idInstall = (int) table.getValueAt(table.getSelectedRow(), 0);
+            	
+                
+	                if(idInstall == -1){
+	                	JOptionPane.showMessageDialog(null, "Aucune selection!", "Erreur", JOptionPane.WARNING_MESSAGE);
+	                }
+                else{
+                	System.out.println(idInstall);
+                	//PreparedStatement prep = MainWindow.conn.prepareStatement("DELETE FROM Installation WHERE Installation.IdInstallation = "+idInstall);
+                	//AccesBDGen.executerInstruction(prep);
+                }
+                JOptionPane.showMessageDialog(null, "l'enregistrement a bien été supprimé", "Suppression réussie", JOptionPane.INFORMATION_MESSAGE);
+                
+            }
+            catch (Exception e){
+            	JOptionPane.showMessageDialog(null, e, "Erreur", JOptionPane.WARNING_MESSAGE);
+            }
+            updateTable(); 
+	}
+	
+	
+    
+	
 		
 	private void updateTable(){
 		try {
-			PreparedStatement prep = MainWindow.conn.prepareStatement("SELECT * FROM Software INNER JOIN Editeur ON Software.CodeEdit=Editeur.CodeEdit INNER JOIN Installation ON Software.CodeSoftware=Installation.CodeSoftware WHERE Designation='"+comboBox.getSelectedItem().toString()+"'");
+			PreparedStatement prep = MainWindow.conn.prepareStatement("SELECT * FROM Installation JOIN Software ON Installation.CodeSoftware=Software.CodeSoftware JOIN Editeur ON Editeur.CodeEdit=Software.CodeEdit  WHERE Designation='"+comboBox.getSelectedItem().toString()+"'");
 			table.setModel(AccesBDGen.creerTableModel(prep));
+			
 		}
 		catch (Exception e1) {
 			JOptionPane.showMessageDialog(null, e1, "Erreur", JOptionPane.WARNING_MESSAGE);
