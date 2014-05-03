53147ec9e9a108441bef8c43195d766a0e4eabbe
Bisounours <Bisounours@10.241.94.145>
2014-05-03 21:19:03 +0200

    search1

--- src/defautPackage/Search1Frame.java
+++ src/defautPackage/Search1Frame.java
@@ -5,16 +5,16 @@ import java.sql.Connection;
 
 import javax.swing.JFrame;
 
+@SuppressWarnings("serial")
 public class Search1Frame extends JFrame {
 	private Container cont;
 	private Search1Pane panel;
 	private Connection conn;
-	@SuppressWarnings("unused")
 	private MainFrame mainFrame;
 	public Search1Frame(MainFrame main){
 		setTitle("Recherche n°1");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setSize(792, 526);
+		setSize(665, 475);
 		setResizable(false);
 		setLocationRelativeTo(null);
 
--- src/defautPackage/Search1Pane.java
+++ src/defautPackage/Search1Pane.java
@@ -1,32 +1,112 @@
 package defautPackage;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import AccesBD.AccesBDGen;
 
 
+
+@SuppressWarnings("serial")
 public class Search1Pane extends JPanel {
 	
 	private JButton btnBackS1;
-	
-	
-	
-	public Search1Pane(Search1Frame search1Frame){
-		this.setBounds(10, 10, 631, 396);
+	private Search1Frame search1Frame;
+	private static JTable tableS;
+	@SuppressWarnings("rawtypes")
+	private JComboBox comboBox;
+	private JLabel lblAnneeEtude;
+	private JLabel lblAnneeEtude2;
+	@SuppressWarnings("unchecked")
+	public Search1Pane(Search1Frame search1Fram){
+		this.setBounds(10, 10, 700, 450);
 		setLayout(null);
+		search1Frame =search1Fram;
+		try {
+			comboBox = new JComboBox(getAnneeEtude());
+
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, e, "Erreur",JOptionPane.WARNING_MESSAGE);
+		}
+		comboBox.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				updateTable();
+			}
+		});
+		comboBox.setBounds(470, 10, 150, 20);
+		this.add(comboBox);
+
 		
-	
+		
+		tableS = new JTable(null);
+		tableS.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		tableS.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		tableS.setBackground(Color.white);
+		updateTable();
+		
 		btnBackS1 = new JButton("Retour");
 		btnBackS1.addActionListener(new Back());
-		btnBackS1.setBounds(553, 361, 65, 23);
+		btnBackS1.setBounds(553, 381, 65, 23);
 		add(btnBackS1);
+		
+		lblAnneeEtude = new JLabel("Année d'");
+		lblAnneeEtude.setBounds(372, 13, 46, 14);
+		this.add(lblAnneeEtude);
+		
+		lblAnneeEtude2 = new JLabel("étude:");
+		lblAnneeEtude2.setBounds(415, 13, 46, 14);
+		this.add(lblAnneeEtude2);
+		
 	}
 	private class Back implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			
+			search1Frame.dispose();
 		}
 	}
+	 private void updateTable() {
+		try {
+			PreparedStatement prepS = search1Frame.getConn().prepareStatement("SELECT * FROM Software JOIN utilisationsoftware ON utilisationSoftware.codesoftware = Software.codesoftware JOIN anneeetude a ON a.idanneeetude = utilisationsoftware.idanneeetude where software.codeinstallation is null and a.annee= '"+ comboBox.getSelectedItem().toString()+"'");			
+			tableS.setModel(AccesBDGen.creerTableModel(prepS));
+			centerJtable(tableS);
+			
+
+		} catch (Exception e1) {
+			JOptionPane.showMessageDialog(null, e1, "Erreur",JOptionPane.WARNING_MESSAGE);
+		}
+	}
+
+	private Object[] getAnneeEtude() throws SQLException {
+		Object[] toReturn = null;
+		PreparedStatement prepS;
+		try {
+			String sqlInstruction="SELECT DISTINCT Annee FROM Anneeetude ";
+			prepS = search1Frame.getConn().prepareStatement(sqlInstruction);
+			
+			toReturn = AccesBDGen.creerListe1Colonne(prepS);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e, "Erreur",JOptionPane.WARNING_MESSAGE);
+		}
+		return toReturn;
+
+	}
+	private void centerJtable(JTable table) {
+		DefaultTableCellRenderer custom = new DefaultTableCellRenderer();
+		custom.setHorizontalAlignment(JLabel.CENTER);
+		for (int i = 0; i < table.getColumnCount(); table.getColumnModel().getColumn(i).setCellRenderer(custom), i++)
+			;
+	}
+  
 }
